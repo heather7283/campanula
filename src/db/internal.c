783ed9160b6d8c613258474a206c7c5118a043f0
heather7283 <heather7283@protonmail.com>
2025-08-09 03:50:58 +0400

    refactor(db/internal): make sql statements more readable

--- src/db/internal.c
+++ src/db/internal.c
@@ -6,126 +6,107 @@
 #include "log.h"
 
 struct sqlite_statement statements[] = {
-    [STATEMENT_ENABLE_FOREIGN_KEYS] = { .source =
-        "PRAGMA foreign_keys = ON",
-    },
+    [STATEMENT_BEGIN] = { .source = "BEGIN" },
+    [STATEMENT_COMMIT] = { .source = "COMMIT" },
+    [STATEMENT_ROLLBACK] = { .source = "ROLLBACK" },
+
+    [STATEMENT_ENABLE_FOREIGN_KEYS] = { .source = "PRAGMA foreign_keys = ON" },
+
     [STATEMENT_CREATE_TABLE_ARTISTS] = { .source =
-        "CREATE TABLE IF NOT EXISTS artists ("
-        "    id TEXT NOT NULL PRIMARY KEY,"
-        "    name TEXT NOT NULL,"
-        ""
-        "    deleted BOOLEAN DEFAULT FALSE"
+        "CREATE TABLE IF NOT EXISTS artists ( "
+            "id TEXT NOT NULL PRIMARY KEY, "
+            "name TEXT NOT NULL, "
+
+            "deleted BOOLEAN DEFAULT FALSE "
         ")"
     },
     [STATEMENT_CREATE_TABLE_ALBUMS] = { .source =
-        "CREATE TABLE IF NOT EXISTS albums ("
-        "    id TEXT NOT NULL PRIMARY KEY,"
-        "    name TEXT NOT NULL,"
-        "    artist TEXT NOT NULL,"
-        "    song_count INTEGER NOT NULL,"
-        "    duration INTEGER NOT NULL,"
-        "    created DATETIME NOT NULL,"
-        ""
-        "    deleted BOOLEAN DEFAULT FALSE,"
-        ""
-        "    artist_id TEXT,"
-        "    FOREIGN KEY(artist_id) REFERENCES artists(id)"
+        "CREATE TABLE IF NOT EXISTS albums ( "
+            "id TEXT NOT NULL PRIMARY KEY, "
+            "name TEXT NOT NULL, "
+            "artist TEXT NOT NULL, "
+            "song_count INTEGER NOT NULL, "
+            "duration INTEGER NOT NULL, "
+            "created DATETIME NOT NULL, "
+
+            "deleted BOOLEAN DEFAULT FALSE, "
+
+            "artist_id TEXT, "
+            "FOREIGN KEY (artist_id) REFERENCES artists (id) "
         ")"
     },
     [STATEMENT_CREATE_TABLE_SONGS] = { .source =
-        "CREATE TABLE IF NOT EXISTS songs ("
-        "    id TEXT NOT NULL PRIMARY KEY,"
-        "    title TEXT NOT NULL,"
-        "    artist TEXT NOT NULL,"
-        "    album TEXT NOT NULL,"
-        ""
-        "    track INTEGER,"
-        "    year INTEGER,"
-        "    duration INTEGER,"
-        "    bitrate INTEGER,"
-        "    size INTEGER,"
-        "    filetype TEXT,"
-        ""
-        "    deleted BOOLEAN DEFAULT FALSE,"
-        ""
-        "    artist_id TEXT,"
-        "    album_id TEXT,"
-        "    FOREIGN KEY(artist_id) REFERENCES artists(id),"
-        "    FOREIGN KEY(album_id) REFERENCES albums(id)"
+        "CREATE TABLE IF NOT EXISTS songs ( "
+            "id TEXT NOT NULL PRIMARY KEY, "
+            "title TEXT NOT NULL, "
+            "artist TEXT NOT NULL, "
+            "album TEXT NOT NULL, "
+
+            "track INTEGER, "
+            "year INTEGER, "
+            "duration INTEGER, "
+            "bitrate INTEGER, "
+            "size INTEGER, "
+            "filetype TEXT, "
+
+            "deleted BOOLEAN DEFAULT FALSE, "
+
+            "artist_id TEXT, "
+            "album_id TEXT, "
+            "FOREIGN KEY (artist_id) REFERENCES artists (id), "
+            "FOREIGN KEY (album_id) REFERENCES albums (id) "
         ")"
     },
 
-    [STATEMENT_BEGIN] = { .source =
-        "BEGIN"
-    },
-    [STATEMENT_COMMIT] = { .source =
-        "COMMIT"
-    },
-    [STATEMENT_ROLLBACK] = { .source =
-        "ROLLBACK"
-    },
-
-    [STATEMENT_MARK_ARTISTS_AS_DELETED] = { .source =
-        "UPDATE artists SET deleted = TRUE"
-    },
-    [STATEMENT_MARK_ALBUMS_AS_DELETED] = { .source =
-        "UPDATE albums SET deleted = TRUE"
-    },
-    [STATEMENT_MARK_SONGS_AS_DELETED] = { .source =
-        "UPDATE songs SET deleted = TRUE"
-    },
-    [STATEMENT_DELETE_DELETED_ARTISTS] = { .source =
-        "DELETE FROM artists WHERE deleted = TRUE"
-    },
-    [STATEMENT_DELETE_DELETED_ALBUMS] = { .source =
-        "DELETE FROM albums WHERE deleted = TRUE"
-    },
-    [STATEMENT_DELETE_DELETED_SONGS] = { .source =
-        "DELETE FROM songs WHERE deleted = TRUE"
-    },
+    [STATEMENT_MARK_ARTISTS_AS_DELETED] = { .source = "UPDATE artists SET deleted = TRUE" },
+    [STATEMENT_MARK_ALBUMS_AS_DELETED] = { .source = "UPDATE albums SET deleted = TRUE" },
+    [STATEMENT_MARK_SONGS_AS_DELETED] = { .source = "UPDATE songs SET deleted = TRUE" },
+    [STATEMENT_DELETE_DELETED_ARTISTS] = { .source = "DELETE FROM artists WHERE deleted = TRUE" },
+    [STATEMENT_DELETE_DELETED_ALBUMS] = { .source = "DELETE FROM albums WHERE deleted = TRUE" },
+    [STATEMENT_DELETE_DELETED_SONGS] = { .source = "DELETE FROM songs WHERE deleted = TRUE" },
 
     [STATEMENT_INSERT_ARTIST] = { .source =
-        "INSERT INTO artists("
-        "   id, name"
-        ") VALUES ("
-        "   $id, $name"
-        ") ON CONFLICT(id) DO UPDATE SET"
-        "   id = excluded.id, name = excluded.name,"
-        "   deleted = FALSE"
+        "INSERT INTO artists ( "
+            "id, name "
+        ") VALUES ( "
+            "$id, $name "
+        ") ON CONFLICT (id) DO UPDATE SET "
+            "id = excluded.id, name = excluded.name, "
+            "deleted = FALSE"
     },
     [STATEMENT_INSERT_ALBUM] = { .source =
-        "INSERT INTO albums("
-        "   id, name, artist, artist_id, song_count, duration, created"
-        ") VALUES ("
-        "   $id, $name, $artist, $artist_id, $song_count, $duration, unixepoch($created)"
-        ") ON CONFLICT(id) DO UPDATE SET"
-        "   id = excluded.id, name = excluded.name,"
-        "   artist = excluded.artist, artist_id = excluded.artist_id,"
-        "   song_count = excluded.song_count, duration = excluded.duration,"
-        "   deleted = FALSE"
+        "INSERT INTO albums ( "
+            "id, name, artist, artist_id, song_count, duration, created "
+        ") VALUES ( "
+            "$id, $name, $artist, $artist_id, $song_count, $duration, unixepoch($created) "
+        ") ON CONFLICT (id) DO UPDATE SET "
+            "id = excluded.id, name = excluded.name, "
+            "artist = excluded.artist, artist_id = excluded.artist_id, "
+            "song_count = excluded.song_count, duration = excluded.duration, "
+            "deleted = FALSE "
     },
     [STATEMENT_INSERT_SONG] = { .source =
-        "INSERT INTO songs("
-        "   id, title, artist, album,"
-        "   track, year, duration, bitrate, size, filetype,"
-        "   artist_id, album_id"
-        ") VALUES ("
-        "   $id, $title, $artist, $album,"
-        "   $track, $year, $duration, $bitrate, $size, $filetype,"
-        "   $artist_id, $album_id"
-        ") ON CONFLICT(id) DO UPDATE SET"
-        "   title = excluded.title,"
-        "   artist = excluded.artist,"
-        "   album = excluded.album,"
-        "   track = excluded.track,"
-        "   year = excluded.year,"
-        "   duration = excluded.duration,"
-        "   bitrate = excluded.bitrate,"
-        "   size = excluded.size,"
-        "   filetype = excluded.filetype,"
-        "   artist_id = excluded.artist_id,"
-        "   album_id = excluded.album_id,"
-        "   deleted = FALSE"
+        "INSERT INTO songs ( "
+            "id, title, artist, album, "
+            "track, year, duration, bitrate, size, filetype, "
+            "artist_id, album_id "
+        ") VALUES ( "
+            "$id, $title, $artist, $album, "
+            "$track, $year, $duration, $bitrate, $size, $filetype, "
+            "$artist_id, $album_id "
+        ") ON CONFLICT (id) DO UPDATE SET "
+            "title = excluded.title, "
+            "artist = excluded.artist, "
+            "album = excluded.album, "
+            "track = excluded.track, "
+            "year = excluded.year, "
+            "duration = excluded.duration, "
+            "bitrate = excluded.bitrate, "
+            "size = excluded.size, "
+            "filetype = excluded.filetype, "
+            "artist_id = excluded.artist_id, "
+            "album_id = excluded.album_id, "
+            "deleted = FALSE "
     },
 
     [STATEMENT_GET_ARTISTS_WITH_PAGINATION] = { .source =
@@ -157,9 +138,9 @@ struct sqlite_statement statements[] = {
 
     [STATEMENT_GET_SONGS_IN_ALBUM] = { .source =
         "SELECT "
-        "   id, title, artist, album,"
-        "   track, year, duration, bitrate, size, filetype,"
-        "   artist_id, album_id "
+            "id, title, artist, album, "
+            "track, year, duration, bitrate, size, filetype, "
+            "artist_id, album_id  "
         "FROM songs "
         "WHERE album_id = $album_id "
         "ORDER BY track ASC"
