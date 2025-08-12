738ab77f13139b33414393dd43f306ef491e03f0
heather7283 <heather7283@protonmail.com>
2025-08-13 00:49:32 +0400

    chore(config): document config struct fields

--- src/config.h
+++ src/config.h
@@ -2,10 +2,16 @@
 #define SRC_CONFIG_H
 
 struct config {
+    /* server url (without /rest) */
     char *server_address;
-    char *application_name;
+    /* server username */
     char *username;
+    /* server passord */
     char *password;
+
+    /* name that will be used in API requests and reported to the audio system */
+    char *application_name;
+
     char *database_path;
 };
 
