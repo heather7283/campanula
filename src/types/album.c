b1c8c3f69a0d0c76fa68388926997174dced463d
heather7283 <heather7283@protonmail.com>
2025-08-28 00:54:35 +0400

    fix(types/album): correct copy behaviour

--- src/types/album.c
+++ src/types/album.c
@@ -2,10 +2,10 @@
 #include "xmalloc.h"
 
 void album_deep_copy(struct album *dst, const struct album *src) {
-    dst->id = xstrdup(dst->id);
-    dst->name = xstrdup(dst->name);
-    dst->artist = xstrdup(dst->artist);
-    dst->artist_id = xstrdup(dst->artist_id);
+    dst->id = xstrdup(src->id);
+    dst->name = xstrdup(src->name);
+    dst->artist = xstrdup(src->artist);
+    dst->artist_id = xstrdup(src->artist_id);
 
     dst->song_count = src->song_count;
     dst->duration = src->duration;
